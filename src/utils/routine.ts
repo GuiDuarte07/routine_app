5480c5f03b301dea761e8ca929d00d8396d0bcbf
Guilherme Duarte <guilduarte07@gmail.com>
2023-07-20 16:08:22 -0300

    change arrayOfSubdiv to objectOfSubdiv

--- src/utils/routine.ts
+++ src/utils/routine.ts
@@ -1,10 +1,35 @@
+
+type ObjectOfSubdividedHours = {
+  subdividedHours: number[];
+  firtHour: number;
+  lastHour: number;
+}
 //Format of startHour and endHour = 14:30
-export function arrayOfSubdividedHours(hourArrays: {startHour: string, endHour: string}[]): number[] {
+export function objectOfSubdividedHours(hourArrays: {startHour: string, endHour: string}[]): ObjectOfSubdividedHours {
   const subdividedHours: number[] = []
+  let firtHour: number = Infinity
+  let lastHour: number = -Infinity
+  let haveMidnight: boolean = false
 
   hourArrays.forEach(({startHour, endHour}) => {
     const [startH, startMin] = startHour.split(":")
     const [endH, endMin] = endHour.split(":")
+
+    if (startH === "00" || endH === "00") {
+      haveMidnight = true
+    }
+
+    if (firtHour === undefined) {
+      firtHour = Number(startH)
+    } else if (firtHour > Number(startH)) {
+      firtHour = Number(startH)
+    }
+
+    if (lastHour === undefined) {
+      lastHour = Number(endH)
+    } else if (lastHour < Number(endH)) {
+      lastHour = Number(endH)
+    }
 
     if (startMin === "30") {
       if (!subdividedHours.find(hour => hour === Number(startH))) {
@@ -18,7 +43,16 @@ export function arrayOfSubdividedHours(hourArrays: {startHour: string, endHour: 
     }
   })
 
+  lastHour += 1
+
+  if (haveMidnight || lastHour === 24) {
+    lastHour = 0
+  }
+
   subdividedHours.sort((a, b) => a - b)
 
-  return subdividedHours
+  return {subdividedHours, firtHour, lastHour}
 }
+
+import example from "@/utils/example.json"
+console.log(objectOfSubdividedHours(example.map(event => ({endHour: event.endHour, startHour: event.startHour}))))
