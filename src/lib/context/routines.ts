62df79723bafd57045fa625b9bb5fa7497bbdeda
Guilherme Duarte <guilduarte07@gmail.com>
2023-07-20 15:29:08 -0300

    state CRUD to routine

--- src/lib/context/routines.ts
+++ src/lib/context/routines.ts
@@ -2,17 +2,40 @@ import example from "@/utils/example.json"
 import { create } from 'zustand'
 
 interface Event {
+  id: number;
   title: string;
   day: string;
   startHour: string;
   endHour: string;
 }
 
-interface Routine {
+interface RoutineStates {
   routine: Event[]
 }
-const useStore = create<Routine>((set) => ({
-  routine: example
+
+interface RoutineActions {
+  addNewEvent: (event: Event) => void
+  deleteEvent: (id: number) => void
+  editEvent: (event: Event) => void
+}
+
+
+const useStore = create<RoutineStates & RoutineActions>((set) => ({
+  routine: example, // Assuming example is an initial array of events
+  addNewEvent: (event) =>
+    set((state) => ({
+      routine: [...state.routine, event], // Add the new event to the routine array
+    })),
+  deleteEvent: (id) =>
+    set((state) => ({
+      routine: state.routine.filter((event) => event.id !== id), // Filter out the event with the given id
+    })),
+  editEvent: (event) =>
+    set((state) => ({
+      routine: state.routine.map((existingEvent) =>
+        existingEvent.id === event.id ? event : existingEvent // Replace the existing event with the edited event
+      ),
+    })),
 }))
 
 export default useStore
